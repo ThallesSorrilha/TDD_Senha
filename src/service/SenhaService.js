e4fa6cb65ade3d9c1b1748f36d85ec71948be1c6
ThallesSorrilha <thallessorrilha@gmail.com>
2026-04-16 20:57:07 -0300

    feat: adicionando verificação de maiúsculas (RED)

--- src/service/SenhaService.js
+++ src/service/SenhaService.js
@@ -1,3 +1,5 @@
+const {constantes} = require("../utils/constantes");
+
 class SenhaService {
   static validarSenha(senha) {
     if (!senha) {
@@ -12,8 +14,20 @@ class SenhaService {
     if (senha.senha.length < 8) {
         return false;
     }
-    
+
     //Pelo menos uma letra maiúscula.
+    requisitoMaiusculo = false;
+    for (let i = 0; i < senha.senha.length; i++) {
+        const element = senha.senha[i];
+        if (constantes.ALFABETO_MAIUSCULO.includes(element)) {
+            requisitoMaiusculo = true;
+            break;
+        }
+    }
+    if (!requisitoMaiusculo) {
+        return false;
+    }
+
     //Pelo menos uma letra minúscula.
     //Pelo menos um número.
     //Pelo menos um caractere especial (ex: !@#$%^&*).
