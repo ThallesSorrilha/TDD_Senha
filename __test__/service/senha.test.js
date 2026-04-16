8a010537b73bd61521c39e5d8309863cc481a364
ThallesSorrilha <thallessorrilha@gmail.com>
2026-04-16 20:07:25 -0300

    feat: adicionando primeiro teste, que é o teste positivo (RED)

--- __test__/service/senha.test.js
+++ __test__/service/senha.test.js
@@ -1,7 +1,14 @@
 describe("Senha", () => {
-  test("Teste ...", () => {
+  test("Teste de senha Válida", () => {
     // Arrange
+    const usuario = new Senha({
+      senha: "Terenc1@",
+    });
+
     // Act
+    const saida = SenhaService.validarSenha(senha);
+
     // Assert
+    expect(true).toBe(saida);
   });
 });
