56240311f2ca08a5358c8f3de839f82d953c1d27
ThallesSorrilha <thallessorrilha@gmail.com>
2026-04-16 20:31:14 -0300

    refactor: verificar se existe atributo senha (RED)

--- src/service/SenhaService.js
+++ src/service/SenhaService.js
@@ -3,7 +3,18 @@ class SenhaService {
     if (!senha) {
       return false;
     }
-    // demais validações
+
+    if (!senha.senha) {
+        return false;
+    }
+    
+    //Mínimo de 8 caracteres.
+    //Pelo menos uma letra maiúscula.
+    //Pelo menos uma letra minúscula.
+    //Pelo menos um número.
+    //Pelo menos um caractere especial (ex: !@#$%^&*).
+    //Não pode conter espaços em branco.
+
     return false;
   }
 }
